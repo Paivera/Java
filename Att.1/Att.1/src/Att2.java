121ff9dec4fe32e92b03f8e13921eca36282a52b
Paiva <111445329+Paivera@users.noreply.github.com>
2024-09-15 19:23:45 -0300

    Add files via upload

---
+++ Att.1/Att.1/src/Att2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Att2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Digite um numero:");
+        Scanner scanner = new Scanner (System.in);
+
+        double numero = scanner.nextDouble();
+        
+        if (numero%2 == 0){
+            System.out.println("Par");
+        }else{
+            System.out.println("Impar");
+        }
+        scanner.close();
+    }
+}
